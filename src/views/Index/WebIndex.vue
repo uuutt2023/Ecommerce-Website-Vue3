173882b16870147077a10f4b73ff18964dbad82d
uuutt <584193570@qq.com>
2023-12-11 23:26:05 +0800

    网站主入口

--- src/views/Index/WebIndex.vue
+++ src/views/Index/WebIndex.vue
@@ -1,44 +1,66 @@
-<!--
- * @Author: uuutt 584193570@qq.com
- * @Date: 2023-12-09 20:03:45
- * @LastEditors: uuutt 584193570@qq.com
- * @LastEditTime: 2023-12-09 22:35:18
- * @FilePath: \vue3-data-pulse\src\views\Index\WebIndex.vue
- * @Description:
- *  网站登录页面
- * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
--->
-
 <template>
-  <div id="wrap">
-    <form>
-
-    </form>
+  <div
+    id="wrap"
+    class="p-lg-5 p-sm-3 container-fluid">
+    <RouterView
+      v-slot="{ Component }"
+      class="input-box p-4 p-md-5 col-xxl-4 col-lg-6 col-md-7 col-11">
+      <KeepAlive>
+        <component :is="Component"></component>
+      </KeepAlive>
+    </RouterView>
   </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 #wrap {
-  height: 100vh;
-  background: url("~@/assets/images/index/bg_light.png") no-repeat center;
-  background-size: auto 100%;
-  overflow: hidden;
+  background: url('~@/assets/images/index/Illustration.png') no-repeat -40% bottom,
+  linear-gradient(to right top, #365dfb, #679cf6) no-repeat;
+  background-size: 970px, 100%;
 
   display: flex;
   align-items: center;
   justify-content: flex-end;
+}
 
-  & form {
-    width: 60vh;
-    aspect-ratio: 19 / 26;
-    background-color: white;
-    box-shadow: 0 10px 40px 0 rgba(0, 0, 0, 0.2);
-    border-radius: 10px;
-    position: relative;
-    right: 7%;
+.input-box {
+  background-color: #ffffff;
+  box-shadow: 0px 10px 40px 0px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+
+  form {
+    background-color: #ffffff;
+    box-shadow: 0px 1px 4px 0px #e5e9f2;
+    border-radius: 5px;
+    margin-top: 40px;
+  }
+
+  header {
+    * {
+      font-weight: normal;
+      font-stretch: normal;
+      letter-spacing: 0px;
+    }
+
+    h2 {
+      color: #31394d;
+    }
+
+    h5 {
+      color: #748aa1;
+    }
+  }
+
+  label {
+    color: #748aa1;
+    text-indent: 0.75em;
+  }
+
+  [type='submit'] {
+    background-color: #29cb97;
+    border-color: #29cb97;
   }
 }
 </style>
