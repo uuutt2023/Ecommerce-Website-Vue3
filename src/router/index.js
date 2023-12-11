2ceeb727299e1c36f38494fcf2e500e0a4aecdcc
uuutt <584193570@qq.com>
2023-12-11 23:23:31 +0800

    添加：“忘记密码 ForgePwd” 子路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,5 @@
-/*
- * @Author: uuutt 584193570@qq.com
- * @Date: 2023-12-09 18:40:57
- * @LastEditors: uuutt 584193570@qq.com
- * @LastEditTime: 2023-12-09 22:01:48
- * @FilePath: \vue3-data-pulse\src\router\index.js
- * @Description:
- *
- * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
- */
-
 import { createRouter, createWebHistory } from 'vue-router';
+import { defineAsyncComponent } from 'vue';
 
 const index = [
   {
@@ -20,17 +10,23 @@ const index = [
     path: '/index',
     name: 'index',
     component: () => import('@/views/Index/WebIndex.vue'),
+    redirect: '/index/signIn',
     meta: { title: '登录界面' },
     children: [
       {
         path: 'signIn',
         name: 'signIn',
-        component: () => import('@/views/Index/components/SignIn.vue'),
+        component: () => defineAsyncComponent(() => import('@/views/Index/components/SignIn.vue')),
       },
       {
         path: 'signUp',
         name: 'signUp',
-        component: () => import('@/views/Index/components/SignUp.vue'),
+        component: () => defineAsyncComponent(() => import('@/views/Index/components/SignUp.vue')),
+      },
+      {
+        path: 'forge',
+        name: 'forge',
+        component: () => defineAsyncComponent(() => import('@/views/Index/components/ForgePwd.vue')),
       },
     ],
   },
---
+++ src/views/Index/components/ForgePwd.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <header>
+      <h2>找回账号</h2>
+      <h5>请输入您的邮箱账号找回密码。</h5>
+    </header>
+    <form class="d-grid my-4 p-3 p-md-4">
+      <div class="mb-3">
+        <label
+          class="form-label"
+          for="username">
+          账号
+        </label>
+        <input
+          id="username"
+          class="form-control"
+          name="username"
+          type="text" />
+      </div>
+      <div class="mb-3">
+        <label
+          class="form-label"
+          for="password1">
+          新密码
+        </label>
+        <input
+          id="password1"
+          class="form-control"
+          name="password1"
+          type="password" />
+      </div>
+      <div>
+        <label
+          class="form-label"
+          for="password2">
+          新密码确认
+        </label>
+        <input
+          id="password2"
+          class="form-control"
+          name="password2"
+          type="password" />
+      </div>
+      <button
+        class="btn btn-success mt-4 p-2"
+        type="submit">
+        找回
+      </button>
+    </form>
+    <footer class="row justify-content-around">
+      <RouterLink
+        class="btn btn-primary col-5 p-2"
+        to="/index/signIn">
+        记得密码啦？
+      </RouterLink>
+      <RouterLink
+        class="btn btn-info col-5 p-2 text-white"
+        to="/index/signUp">
+        重新注册
+      </RouterLink>
+    </footer>
+  </div>
+</template>
