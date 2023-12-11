8e2b5c585013430588e12b65e407c677765e49b7
uuutt <584193570@qq.com>
2023-12-11 19:03:58 +0800

    还原版本

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,4 @@
-<!--
- * @Author: uuutt 584193570@qq.com
- * @Date: 2023-12-09 18:40:31
- * @LastEditors: uuutt 584193570@qq.com
- * @LastEditTime: 2023-12-09 22:20:40
- * @FilePath: \vue3-data-pulse\src\App.vue
- * @Description:
- *  根组件
- * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
--->
+
 <template>
   <RouterView></RouterView>
 </template>
--- src/views/Index/components/SignUp.vue
+++ src/views/Index/components/SignUp.vue
@@ -1,13 +1,3 @@
-<!--
- * @Author: uuutt 584193570@qq.com
- * @Date: 2023-12-09 20:04:27
- * @LastEditors: uuutt 584193570@qq.com
- * @LastEditTime: 2023-12-09 21:55:17
- * @FilePath: \vue3-data-pulse\src\views\Index\components\SignUp.vue
- * @Description:
- *  注册视图
- * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
--->
 
 <template>
   <div>注册视图</div>
