efea49626d093ca30a54f9d4e92df9b564de34c7
uuutt <584193570@qq.com>
2023-12-11 23:24:22 +0800

    引入：bootStrap 5

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+
+// BootStrap 5
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 createApp(App).use(router).mount('#app');
