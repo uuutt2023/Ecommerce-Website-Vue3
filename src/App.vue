ddf6e44ecae125dce4c96961cbd93fc27e2d0e3e
uuutt <584193570@qq.com>
2023-12-11 23:21:51 +0800

    添加视图公共类

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
-
 <template>
-  <RouterView></RouterView>
+  <RouterView class="view"></RouterView>
 </template>
 
 <script setup></script>
@@ -10,5 +9,10 @@
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+
+.view {
+  height: 100vh;
+  overflow: hidden;
 }
 </style>
