c85390b3234cbf5893353d600b145119eb2b5b58
uuutt <584193570@qq.com>
2023-12-12 18:03:33 +0800

    输入框组件化

--- src/views/Index/components/SignIn.vue
+++ src/views/Index/components/SignIn.vue
@@ -1,53 +1,43 @@
-<template>
-  <div>
-    <header>
-      <h2>登录</h2>
-      <h5>请输入您的账号与密码</h5>
-    </header>
-    <form class="d-grid my-4 p-3 p-md-4">
-      <div class="mb-3">
-        <label
-          class="form-label"
-          for="username">
-          账号
-        </label>
-        <input
-          id="username"
-          class="form-control"
-          name="username"
-          type="text" />
-      </div>
-      <div>
-        <label
-          class="form-label"
-          for="password">
-          密码
-        </label>
-        <input
-          id="password"
-          class="form-control"
-          name="password"
-          type="password" />
-      </div>
-      <button
-        class="btn btn-success mt-4 p-2"
-        type="submit">
-        登录
-      </button>
-    </form>
-    <footer class="row justify-content-around">
-      <RouterLink
-        class="btn btn-primary col-5 p-2"
-        to="/index/signUp">
-        新用户?
-      </RouterLink>
-      <RouterLink
-        class="btn btn-info col-5 p-2 text-white"
-        to="/index/forge">
-        忘了？
-      </RouterLink>
-    </footer>
-  </div>
-</template>
+<script setup>
+import { Form as VeeForm } from 'vee-validate';
+import VeeInput from '@/views/Index/components/VeeInput.vue';
+</script>
 
-<script setup></script>
+<template>
+  <header>
+    <h2>登录</h2>
+    <h5>请输入您的账号与密码</h5>
+  </header>
+  <!-- 验证表单 -->
+  <VeeForm class="d-grid my-4 p-3 p-md-4">
+    <VeeInput
+      class="mb-3"
+      label-str="账号"
+      name="username"
+      rules="required"
+      type="text" />
+    <VeeInput
+      label-str="密码"
+      name="password"
+      rules="required"
+      type="password" />
+    <button
+      class="btn btn-success mt-4 p-2"
+      type="submit">
+      登录
+    </button>
+  </VeeForm>
+  <!-- 跳转按钮 -->
+  <footer class="row justify-content-around">
+    <RouterLink
+      class="btn btn-primary col-5 p-2"
+      to="/index/signUp">
+      新用户?
+    </RouterLink>
+    <RouterLink
+      class="btn btn-info col-5 p-2 text-white"
+      to="/index/forge">
+      忘了？
+    </RouterLink>
+  </footer>
+</template>
--- src/views/Index/components/SignUp.vue
+++ src/views/Index/components/SignUp.vue
@@ -1,70 +1,50 @@
-<template>
-  <div>
-    <header>
-      <h2>注册</h2>
-      <h5>没有账号？那就注册一个吧。</h5>
-    </header>
-    <form class="d-grid my-4 p-3 p-md-4">
-      <div class="mb-3">
-        <label
-          class="form-label"
-          for="username">
-          账号
-        </label>
-        <ValidationProvider>
-          <input
-            id="username"
-            v-model="value"
-            class="form-control"
-            name="username"
-            type="text" />
-        </ValidationProvider>
-      </div>
-      <div class="mb-3">
-        <label
-          class="form-label"
-          for="password1">
-          密码
-        </label>
-        <input
-          id="password1"
-          class="form-control"
-          name="password1"
-          type="password" />
-      </div>
-      <div>
-        <label
-          class="form-label"
-          for="password2">
-          密码确认
-        </label>
-        <input
-          id="password2"
-          class="form-control"
-          name="password2"
-          type="password" />
-      </div>
-      <button
-        class="btn btn-success mt-4 p-2"
-        type="submit">
-        立即注册
-      </button>
-    </form>
-    <footer class="row justify-content-around">
-      <RouterLink
-        class="btn btn-primary col-5 p-2"
-        to="/index/signIn">
-        有账号啦？
-      </RouterLink>
-      <RouterLink
-        class="btn btn-info col-5 p-2 text-white"
-        to="/index/forge">
-        忘了？
-      </RouterLink>
-    </footer>
-  </div>
-</template>
+<script setup>
+import VeeInput from '@/views/Index/components/VeeInput.vue';
+import { Form } from 'vee-validate';
+</script>
 
-<script setup></script>
+<template>
+  <header>
+    <h2>注册</h2>
+    <h5>没有账号？那就注册一个吧。</h5>
+  </header>
+  <Form class="d-grid my-4 p-3 p-md-4">
+    <VeeInput
+      :label-str="$t('username')"
+      class="mb-3"
+      name="username"
+      rules="minLength:6|required"
+      type="text" />
+    <VeeInput
+      :label-str="$t('password_1')"
+      class="mb-3"
+      name="password1"
+      rules="required"
+      type="password" />
+    <VeeInput
+      :label-str="$t('password_2')"
+      class="mb-3"
+      name="password2"
+      rules="required"
+      type="password" />
+    <button
+      class="btn btn-success mt-4 p-2"
+      type="submit">
+      立即注册
+    </button>
+  </Form>
+  <footer class="row justify-content-around">
+    <RouterLink
+      class="btn btn-primary col-5 p-2"
+      to="/index/signIn">
+      有账号啦？
+    </RouterLink>
+    <RouterLink
+      class="btn btn-info col-5 p-2 text-white"
+      to="/index/forge">
+      忘了？
+    </RouterLink>
+  </footer>
+</template>
 
 <style lang="scss" scoped></style>
