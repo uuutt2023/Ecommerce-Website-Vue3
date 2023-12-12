07652ab236a0624e22c0a9bbca086bbb4d1345bd
uuutt <584193570@qq.com>
2023-12-12 18:04:52 +0800

    可选缓存路由

--- src/views/Index/WebIndex.vue
+++ src/views/Index/WebIndex.vue
@@ -1,23 +1,27 @@
+<script setup></script>
+
 <template>
   <div
     id="wrap"
     class="p-lg-5 p-sm-3 container-fluid">
-    <RouterView
-      v-slot="{ Component }"
-      class="input-box p-4 p-md-5 col-xxl-4 col-lg-6 col-md-7 col-11">
-      <KeepAlive>
-        <component :is="Component"></component>
-      </KeepAlive>
-    </RouterView>
+    <div class="input-box p-4 p-md-5 col-xxl-4 col-lg-6 col-md-7 col-10">
+      <RouterView v-slot="{ Component }">
+        <KeepAlive>
+          <component
+            :is="Component"
+            v-if="$route.meta.keepAlive" />
+        </KeepAlive>
+      </RouterView>
+      <RouterView v-if="!$route.meta.keepAlive"></RouterView>
+    </div>
   </div>
 </template>
 
-<script setup></script>
-
 <style lang="scss">
 #wrap {
-  background: url('~@/assets/images/index/Illustration.png') no-repeat -40% bottom,
-  linear-gradient(to right top, #365dfb, #679cf6) no-repeat;
+  background:
+    url('~@/assets/images/index/Illustration.png') no-repeat -40% bottom,
+    linear-gradient(to right top, #365dfb, #679cf6) no-repeat;
   background-size: 970px, 100%;
 
   display: flex;
@@ -27,12 +31,12 @@
 
 .input-box {
   background-color: #ffffff;
-  box-shadow: 0px 10px 40px 0px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 10px 40px 0 rgba(0, 0, 0, 0.2);
   border-radius: 5px;
 
   form {
     background-color: #ffffff;
-    box-shadow: 0px 1px 4px 0px #e5e9f2;
+    box-shadow: 0 1px 4px 0 #e5e9f2;
     border-radius: 5px;
     margin-top: 40px;
   }
@@ -51,11 +55,6 @@
     h5 {
       color: #748aa1;
     }
-  }
-
-  label {
-    color: #748aa1;
-    text-indent: 0.75em;
   }
 
   [type='submit'] {
