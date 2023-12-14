feeb3efb09bba8a7919c6551faf18145a5b43cad
uuutt <584193570@qq.com>
2023-12-14 22:15:45 +0800

    采用 yup 格式验证插件，移除旧规则

--- src/validation.js
+++
@@ -1,4 +0,0 @@
-import { defineRule } from 'vee-validate';
-
-defineRule('required', (value) => (value && value.trim() ? true : '不能为空'));
-defineRule('minLength', (value, [len]) => (value && value.length >= len ? true : `请输入 ${len} 个及以上的字符`));
