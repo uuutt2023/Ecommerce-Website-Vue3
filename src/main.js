8b0f8748d9c7b46d41e164e20d6df59a3d9f6ab0
uuutt <584193570@qq.com>
2023-12-16 12:46:36 +0800

    添加动画库，图标库，Mock, Vuex

--- src/main.js
+++ src/main.js
@@ -3,14 +3,24 @@ import App from './App.vue';
 
 // Vue 路由
 import router from './router';
-
+// VueX
+import store from './store';
+// Mock.js 模拟数据接口
+import '@/mock/index';
 // BootStrap 5
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+// 动画库
+import 'animate.css';
+// 图标库
+import '@/assets/icon/iconfont.css';
 
 // 国际化整合
-import i18n from '@/i18n';
+import { i18n } from '@/i18n';
 
 let app = createApp(App);
 
-app.use(i18n).use(router).mount('#app');
+app.use(i18n);
+app.use(store);
+app.use(router);
+app.mount('#app');
