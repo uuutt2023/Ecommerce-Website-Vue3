3fd5dc196ffa52993d487d3ce74c0c9a60d370ec
uuutt <584193570@qq.com>
2023-12-16 12:44:26 +0800

    不刷新同时切换语言

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
-<template>
-  <RouterView class="view"></RouterView>
-</template>
+<script setup>
+import { nextTick, ref, watch } from 'vue';
+import { i18n } from '@/i18n';
 
-<script setup></script>
+const isRouterAlive = ref(true);
+
+watch(i18n.global.locale, () => {
+  isRouterAlive.value = false;
+  nextTick(() => {
+    isRouterAlive.value = true;
+  });
+});
+</script>
+
+<template>
+  <div class="container view p-0">
+    <RouterView v-if="isRouterAlive" />
+  </div>
+</template>
 
 <style lang="scss">
 * {
@@ -14,5 +28,7 @@
 .view {
   height: 100vh;
   overflow: hidden;
+  background-color: #fff;
+  box-shadow: 0 0 20px #bcbcbc;
 }
 </style>
