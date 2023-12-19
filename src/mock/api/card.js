23bd8842d8a7a86f914312002ebf0d34ccd23209
uuutt <584193570@qq.com>
2023-12-19 16:37:03 +0800

    模拟后端逻辑更正

--- src/mock/api/card.js
+++ src/mock/api/card.js
@@ -1,30 +1,18 @@
-import card from '@/mock/node/cardInfo.json';
-import catImg from '@/mock/node/catImg.json';
-import { getUrlQueryParams } from '@/assets/js/util';
+import cardInfoJson from '@/mock/node/cardInfo.json';
+import catImgJson from '@/mock/node/catImg.json';
 
 /**
- * 卡片所有数据
+ * 卡片缩略图渲染
  * */
-export const GetListCardInfo = [
+export const GetListCardList = [
   {
     name: 'cardInfo',
-    url: /\/api\/card\/info/,
-    path: '/api/card/info',
-    todo: (req) => {
-      let infoArray = [...card.data.cardInfo];
-      let { num } = getUrlQueryParams(req.url);
-
-      let result = [];
-      do {
-        result.push(...infoArray.slice(0, num < 8 ? num : 8));
-        console.log(num, result);
-        num -= 8;
-      } while (num >= 8);
-
-      return result.map((item, index) => ({
-        ...item,
-        ...catImg.data[index],
-      }));
+    type: 'get',
+    url: /\/api\/card\/list/,
+    path: '/api/card/list',
+    todo: () => {
+      const [{ cardInfo }, { catImg }] = [cardInfoJson.data, catImgJson.data];
+      return cardInfo.map((item, index) => ({ ...item, ...catImg[index] }));
     },
   },
 ];
@@ -38,10 +26,10 @@ export const GetCardDetail = [
   {
     name: 'cardDetail',
     type: 'get',
-    url: /\/api\/card\/detail/,
-    path: '/api/card/detail',
+    url: /\/api\/detail\/cat/,
+    path: '/api/detail/cat',
     todo: (resp) => {
-      console.log(resp, card);
+      console.log(resp);
     },
   },
 ];
