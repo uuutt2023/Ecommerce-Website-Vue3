a90a1f9dd30ed6abf3104e6f1aad6c037445c152
uuutt <584193570@qq.com>
2023-12-19 16:36:01 +0800

    用户主页布局修改

--- src/views/user/components/UserHome.vue
+++ src/views/user/components/UserHome.vue
@@ -1,42 +1,17 @@
 <script setup>
 import TopBar from '@/components/TopBar.vue';
-import BaseCard from '@/views/user/components/BaseCard.vue';
-import { nextTick, ref, watch } from 'vue';
-import axios from 'axios';
-import { throttle } from '@/assets/js/util';
-
-// 渲染数量
-let readerNum = ref(0);
-
-// 渲染卡片列表
-let listReader = ref([]);
-
-// 监听渲染数量
-watch(
-  readerNum,
-  async () => {
-    const resp = await axios.get(`/api/card/info?num=${readerNum.value}`);
-    await nextTick(() => listReader.value.push(...resp.data));
-  },
-  { immediate: true },
-);
-
-const addMoreImages = () => {
-  throttle(() => {
-    console.log('滚动');
-    readerNum.value += 8;
-  }, 2000)();
-};
+import WaterFall from '@/components/WaterFall.vue';
+import BottomBar from '@/components/BottomBar.vue';
 </script>
 
 <template>
   <TopBar>
     <template #left>
-      <i class="iconfont icon-back me-auto" />
+      <i class="iconfont icon-back" />
     </template>
     <template #title></template>
     <template #right>
-      <i class="iconfont icon-set-up" />
+      <i class="iconfont icon-set-up me-3" />
       <i class="iconfont icon-search" />
     </template>
     <template #section>
@@ -49,45 +24,18 @@ const addMoreImages = () => {
         class="avatar"
         src="~@/assets/images/user/Oval.png" />
     </template>
-    <template #nav></template>
   </TopBar>
-  <section class="p-0">
-    <div
-      v-if="listReader"
-      v-infinite-scroll="addMoreImages"
-      class="row justify-content-evenly">
-      <BaseCard
-        v-for="(item, index) in listReader"
-        :key="index"
-        :list="item"
-        class="col-5" />
-    </div>
-    <!-- 上滑更新 -->
-    <footer class="mx-auto my-3">
-      <i class="spinner-border" />
-    </footer>
-  </section>
+
+  <WaterFall
+    :click-url="'/api/detail/cat'"
+    :count="6"
+    :url="'/api/card/list'" />
+
+  <BottomBar />
 </template>
 
 <style lang="scss" scoped>
 @use '~@/assets/scss/utils';
-
-section {
-  overflow-y: scroll;
-  flex-grow: 1;
-  border-top: 2px solid #e6e6e7;
-
-  footer {
-    @extend %flex-center;
-  }
-
-  &::after {
-    content: '';
-    display: block;
-    flex: 1 1 auto;
-    width: 30%;
-  }
-}
 
 .avatar {
   width: 44px;
@@ -110,5 +58,9 @@ h1 {
     line-height: 18px;
     text-align: left;
   }
+}
+
+.iconfont {
+  font-size: 24px;
 }
 </style>
