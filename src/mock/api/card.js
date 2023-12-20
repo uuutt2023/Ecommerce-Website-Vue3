ba96cfd05c5153bd1a9a310f0147a4036f78c3cd
uuutt <584193570@qq.com>
2023-12-20 16:18:37 +0800

    固定图片数据

--- src/mock/api/card.js
+++ src/mock/api/card.js
@@ -1,8 +1,15 @@
 import catImgJson from '@/mock/node/catImg.json';
-import Mock from 'mockjs';
+import cardInfoJson from '@/mock/node/cardInfo.json';
+import { getUrlQueryParams } from '@/assets/js/util';
 
-const catImg = catImgJson.data;
-console.log(catImg);
+const catImg = catImgJson.data,
+  { cardInfo } = cardInfoJson.data;
+
+const catInfo = cardInfo.map((item, index) => ({ ...item, ...catImg[index] }));
+
+/**
+ * 图片预加载
+ * */
 [...catImg].forEach(async (item) => {
   const img = new Image();
   img.src = item.url;
@@ -17,37 +24,12 @@ export const GetListCardList = [
     type: 'get',
     url: /\/api\/card\/list/,
     path: '/api/card/list',
-    todo: () => {
-      const cardInfo = Mock.mock({
-        'cats|100': [
-          {
-            animal: '猫',
-            'breed|1': [
-              '英短猫',
-              '布偶猫',
-              '波斯猫',
-              '暹罗猫',
-              '缅因猫',
-              '挪威森林猫',
-              '孟买猫',
-              '俄罗斯蓝猫',
-            ],
-            description: '@cword(10, 20)',
-            'fur_color|1': ['蓝色', '奶白色', '灰色', '黑色', '橘色', '棕色', '斑点', '三花'],
-            name: '@cname',
-            title: '@ctitle(5, 10)',
-            'user_messages|2-5': [
-              {
-                'uid|1-40': 1,
-                sender: '@cname',
-                message: '@cparagraph(1, 3)',
-              },
-            ],
-          },
-        ],
-      });
-      return cardInfo?.cats.map((item, index) => ({ ...item, ...catImg[index] }));
-    },
+    todo: () =>
+      catInfo.map((item) => ({
+        id: item.id,
+        title: item.title,
+        url: item.url,
+      })),
   },
 ];
 
@@ -62,8 +44,9 @@ export const GetCardDetail = [
     type: 'get',
     url: /\/api\/detail\/cat/,
     path: '/api/detail/cat',
-    todo: (resp) => {
-      console.log(resp);
+    todo: () => {
+      const { id } = getUrlQueryParams();
+      return catInfo.find((item) => item.id === id);
     },
   },
 ];
